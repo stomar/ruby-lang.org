3342508a6d3190e04537209a57d8fffc2194b992
Jean-Denis Vauguet <jd@vauguet.fr>
2011-09-04 02:14:31 +0200

    track public directory

---
+++ public/config.rb
@@ -0,0 +1,9 @@
+# Compass configuration
+#
+http_path       = "/"
+css_dir         = "stylesheets"
+sass_dir        = "sass"
+images_dir      = "images"
+javascripts_dir = "javascripts"
+output_style    = :expanded # :compact or :compressed in prod
+# line_comments = false
